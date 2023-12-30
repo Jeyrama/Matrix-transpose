173f291e9bc13b34c5398b8bc320fba98d99bb9a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-30 10:02:50 -0800

    Update Matrix-transpose.js

--- Matrix-transpose.js
+++ Matrix-transpose.js
@@ -35,3 +35,11 @@ function transpose(matrix) {
 }
 
 // or
+
+function transpose(matrix) {
+  return matrix[0].map(function(_, i) {
+    return matrix.map(function(_, j) {
+      return matrix[j][i];
+    });
+  });
+}
