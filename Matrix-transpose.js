be47f83f84967d2f6941752afe0dc47394554fa8
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-30 10:02:08 -0800

    Update Matrix-transpose.js

--- Matrix-transpose.js
+++ Matrix-transpose.js
@@ -19,3 +19,17 @@ For example, the transpose of:
 
 
 // Solution
+
+function transpose(matrix) {
+  let transposed = [],
+    rows = matrix.length,
+    cols = matrix[0].length;
+   
+  for (i = 0; i < cols; i++) {
+  transposed[i] = [];
+    for (let j = 0; j < rows; j++) {
+      transposed[i][j] = matrix[j][i];
+    }
+  }
+  return transposed;
+}
